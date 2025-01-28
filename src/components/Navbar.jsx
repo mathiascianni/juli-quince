676cc409e006e1123305cfc1e0deda52809aba86
Mathias <mncianni@hotmail.com>
2025-01-28 20:10:49 -0300

    fix: last changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -34,9 +34,9 @@ const Navbar = () => {
   };
 
   return (
-    <nav className={`z-40 fixed flex items-center gap-4 xl:px-desk bg-indigo-700 px-4 py-4 w-full transition-all duration-500 ${isShowing ? 'translate-y-0' : '-translate-y-full'}`}>
+    <nav className={`z-40 fixed flex items-center gap-4 xl:px-desk bg-indigo-700 px-4 font-body py-4 w-full transition-all duration-500 ${isShowing ? 'translate-y-0' : '-translate-y-full'}`}>
       <div className="flex justify-between items-center w-full">
-        <a href="#incio">July</a>
+        <a href="#incio" className="relative flex justify-center items-center gap-2 font-title"><img src="icons/star.svg" alt="" className="w-6" />July</a>
         <button
           onClick={toggleMenu}
           className="block xl:hidden"
